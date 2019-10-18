7b59a647998c431baf8a62919eebf4e19bc211fe
ChanHoHan <chhan2759@gmail.com>
2019-10-18 17:13:03 +0900

    Updates

---
+++ precache-manifest.03e6014895f971ba4c2ad38f9a261a98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2dd6deface25e5138e81e49897c067e",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "800778e09d77a02e34d8",
+    "url": "/movie_app/static/css/main.f8961b59.chunk.css"
+  },
+  {
+    "revision": "dcb22c57e3716c392538",
+    "url": "/movie_app/static/js/2.70d9cbc6.chunk.js"
+  },
+  {
+    "revision": "800778e09d77a02e34d8",
+    "url": "/movie_app/static/js/main.06d96408.chunk.js"
+  },
+  {
+    "revision": "9b07edc508fa4bfefff7",
+    "url": "/movie_app/static/js/runtime-main.ab878cc8.js"
+  }
+]);
